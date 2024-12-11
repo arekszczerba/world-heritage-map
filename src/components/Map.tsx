4508d8bd7eeef7dd44e4a0f4b4fc82875ea79613
Szczerba <aszczerba@deloittece.com>
2024-12-11 10:17:53 +0100

    Fix map styling

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,7 +5,7 @@ const Map = () => {
 	const position = [51.7556223, 19.4725837]
 
 	return (
-		<MapContainer center={position} zoom={2} style={{ height: "100vh", width: "100%" }}>
+		<MapContainer center={position} zoom={2} style={{ height: "70vh", width: "100%" }}>
 			<TileLayer
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
