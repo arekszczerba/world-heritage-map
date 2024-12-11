c498710f340cb2156ff9cfd10896955579ac8b3f
Szczerba <aszczerba@deloittece.com>
2024-12-11 11:08:48 +0100

    Adding CSV data parsing and display markers on map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,20 +1,63 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { parseCSV } from '../services/api';
+
+// TODO: Add more fileds?
+type Location = {
+	id_no: string;
+	name_en: string;
+	latitude: string;
+	longitude: string;
+	category: string;
+}
 
 const Map = () => {
-	const position = [51.7556223, 19.4725837]
+  const [locations, setLocations] = useState<Location[]>([]);
 
-	return (
-		<MapContainer center={position} zoom={2} style={{ height: "70vh", width: "100%" }}>
-			<TileLayer
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
-				
-				<Marker position={position}>
-        	<Popup>Default Location Lodz</Popup>
-      	</Marker>
-    	</MapContainer>
-	)
-}
+	useEffect(() => {
+		// CSV Read
+		const fetchData = async() => {
+			try {
+				const response = await fetch('/whc-sites.csv');
+				const csvText = await response.text();
+				const data = (await parseCSV(csvText)) as Location[];
+				setLocations(data);
+			} catch(error) {
+				console.error('Eror parsing CSV:', error)
+			}
+		};
+
+		fetchData();
+	}, []);
+
+  return (
+    <MapContainer
+      center={[20, 0]}
+      zoom={2}
+      style={{ height: "70vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+			{locations.filter((location) => !isNaN(parseFloat(location.latitude)) && !isNaN(parseFloat(location.longitude))).map((location) => (
+
+				<Marker
+					key={location.id_no}
+					position={[parseFloat(location.latitude),parseFloat(location.longitude)]}
+				>
+					
+					<Popup>
+            <strong>{location.name_en}</strong>
+            <br />
+            Category: {location.category}
+          </Popup>
+				</Marker>
+
+			))};
+
+    </MapContainer>
+  );
+};
 
 export default Map;
